15679b8f6e61cd00c8f7a4069374da4cdcca36b0
Rita Rodrigues <rita@mrdevit.com>
2024-09-03 16:29:32 +0100

    remove all ecommerce not going to implement it

--- src/main/java/com/main/bitebyte/ecommerce/EcommerceProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.main.bitebyte.ecommerce;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EcommerceProductRepository extends MongoRepository<EcommerceProduct, String> {
-    // Add custom query methods if needed
-}
